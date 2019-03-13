f65ff28174e9cfadf54fcc7ff129fb0e37847765
Brian Cochrane <brian.cochrane@nike.com>
2019-03-12 21:02:37 -0700

    initial commit

---
+++ program.py
@@ -0,0 +1,9 @@
+print('-----------------------------------------')
+print('              HELLO APP')
+print('-----------------------------------------')
+print()
+
+user_text = input('What is your name?')
+greeting = 'Guten Tag, '
+
+print(greeting + user_text)
